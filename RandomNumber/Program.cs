1d69c65d43f215d5c7785f8f1bcf4d2a48279239
Marten. <marten.aun@tkt.ee>
2024-09-27 21:05:59 +0300

    add Week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Random rnd = new Random();
+
+int myrandomnum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    
+   
+    myrandomnum = rnd.Next(0, 11);
+    randomSum = randomSum + myrandomnum;
+    Console.WriteLine($"My random number is: {myrandomnum}");
+}
+Console.WriteLine($"Random Sum: {randomSum}");
